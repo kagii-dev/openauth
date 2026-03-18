bc4a25dd495a97264b6b2f1aa4662a583e13a799
Justin <shpaw415@gmail.com>
2026-03-18 17:11:04 -0400

    fix the index.js export no sources.

--- packages/openauth/src/index.ts
+++ packages/openauth/src/index.ts
@@ -1,26 +1,22 @@
-export {
-  /**
-   * @deprecated
-   * Use `import { createClient } from "@kagii/openauth/client"` instead - it will tree shake better
-   */
-  createClient,
-} from "./client.js"
+import { createClient as _createClient } from "./client.js"
+import { issuer as _issuer } from "./issuer.js"
+import { createSubjects as _createSubjects } from "./subject.js"
 
-export {
-  /**
-   * @deprecated
-   * Use `import { createSubjects } from "@kagii/openauth/subject"` instead - it will tree shake better
-   */
-  createSubjects,
-} from "./subject.js"
-
-import { issuer } from "./issuer.js"
-
-export {
-  /**
-   * @deprecated
-   * Use `import { issuer } from "@kagii/openauth"` instead, it was renamed
-   */
-  issuer as authorizer,
-  issuer,
+const to_exports = {
+  issuer: _issuer,
+  createClient: _createClient,
+  createSubjects: _createSubjects,
 }
+
+export const issuer = to_exports.issuer
+export const createClient = to_exports.createClient
+/**
+ * @deprecated
+ * Use `import { createSubjects } from "@kagii/openauth/subject"` instead - it will tree shake better
+ */
+export const createSubjects = to_exports.createSubjects
+/**
+ * @deprecated
+ * Use `import { issuer } from "@kagii/openauth"` instead, it was renamed
+ */
+export const authorizer = to_exports.issuer
